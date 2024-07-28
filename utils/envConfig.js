7cb3479d9db184aa84a3f0a61cf163c25670e335
Kushan- <kushan.joshi@outlook.com>
2024-07-28 00:15:09 -0400

    tracking env error on vercel

--- utils/envConfig.js
+++ utils/envConfig.js
@@ -1,3 +1,6 @@
+console.log(process.env)
+console.info(process.env)
+
 export const NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
 export const CLERK_SECRET_KEY=process.env.CLERK_SECRET_KEY
 export const NEXT_PUBLIC_CLERK_SIGN_IN_URL=process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL
