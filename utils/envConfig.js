3461b90b8d702f7bcf7721194f6243a63a3cb0b6
Kushan- <kushan.joshi@outlook.com>
2024-07-28 00:24:32 -0400

    vercel getting env-tracked loh to log

--- utils/envConfig.js
+++ utils/envConfig.js
@@ -1,6 +1,6 @@
 console.log(process.env)
 console.info(process.env)
-console.loh("-====================")
+console.log("-====================")
 
 export const NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
 export const CLERK_SECRET_KEY=process.env.CLERK_SECRET_KEY
